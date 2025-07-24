4df4bf90b288ce46ba0de57db578ca4388a1b47c
Akshat kansal <akshatkansal951@gmail.com>
2025-07-24 23:26:51 +0530

    Update ResumeAnalysisController.java

--- backend/ResCall/src/main/java/com/backend/ResCall/controller/ResumeAnalysisController.java
+++ backend/ResCall/src/main/java/com/backend/ResCall/controller/ResumeAnalysisController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:1234")
+@CrossOrigin(origins = "https://goel-ansh.github.io/ResCall/")
 public class ResumeAnalysisController {
     private final ResumeAnalysisService analysisService;
 
