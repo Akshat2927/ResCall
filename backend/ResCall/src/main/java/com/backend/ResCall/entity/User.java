4c9b7bd0401590424a246834ce9ce8c8d0a6eb7c
Akshat kansal <akshatkansal951@gmail.com>
2025-07-26 21:22:51 +0530

    Update User.java

--- backend/ResCall/src/main/java/com/backend/ResCall/entity/User.java
+++ backend/ResCall/src/main/java/com/backend/ResCall/entity/User.java
@@ -2,6 +2,7 @@ package com.backend.ResCall.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.data.mongodb.core.index.Indexed;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 
@@ -12,6 +13,8 @@ public class User {
     @Id
     private String id;
     private String name;
+
+    @Indexed(unique = true)
     private String email;
     private String password;
 }
